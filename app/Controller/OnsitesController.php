26095565a343566e2527412e42fb595cdee1f2be
Prasaath <prasaath.samsys@gmail.com>
2014-08-20 14:35:18 +0530

    onsite changes

--- app/Controller/OnsitesController.php
+++ app/Controller/OnsitesController.php
@@ -8,9 +8,9 @@
 class OnsitesController extends AppController
 {
     public $helpers = array('Html','Form','Session');
-//    public $uses =array('Priority','Paymentterm','Quotation','Currency',
-//                            'Country','Additionalcharge','Service','CustomerInstrument','Customerspecialneed',
-//                            'Instrument','Brand','Customer','Device','Salesorder','Description','Logactivity');   
+    public $uses =array('Priority','Paymentterm','Quotation','Currency',
+                            'Country','Additionalcharge','Service','CustomerInstrument','Customerspecialneed',
+                            'Instrument','Brand','Customer','Device','Salesorder','Description','Logactivity','Datalog');   
     
     public function index()
     {
@@ -20,6 +20,33 @@ class OnsitesController extends AppController
     {
         
     }
+    
+    public function search()
+        {
+            $this->loadModel('Customer');
+            $name =  $this->request->data['name'];
+            $this->autoRender = false;
+            $data = $this->Customer->find('all',array('conditions'=>array('Customertagname LIKE'=>'%'.$name.'%','Customer.is_deleted'=>0,'Customer.status'=>1)));
+            $c = count($data);
+            if($c>0)
+            {
+                for($i = 0; $i<$c;$i++)
+                { 
+                    echo "<div class='customer_show' align='left' id='".$data[$i]['Customer']['id']."'>";
+                    echo $data[$i]['Customer']['Customertagname'];
+                    echo "<br>";
+                    echo "</div>";
+                }
+            }
+            else
+            {
+                    echo "<div class='no_result' align='left'>";
+                    echo "No Results Found";
+                    echo "<br>";
+                    echo "</div>";
+            }
+            
+        }
 }
     
 ?>
