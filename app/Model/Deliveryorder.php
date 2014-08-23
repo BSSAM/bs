b713e31faa9813f49da313e184dc32a81fb051f2
Prasaath <prasaath.samsys@gmail.com>
2014-08-23 21:48:51 +0530

    Revert "Revert "changes in delivery order""
    This reverts commit 2e28e34c11538842eb1fd0ebfd1febd0d9fcc0aa.

--- app/Model/Deliveryorder.php
+++ app/Model/Deliveryorder.php
@@ -9,6 +9,7 @@ class Deliveryorder extends AppModel
 {
     //public $actsAs  =   'Containable';
     var $name   =   'Deliveryorder';
+    //public $belongsTo  =   ;
     public $hasMany = array(
         'DelDescription' => array(
             'className' => 'DelDescription',
@@ -34,6 +35,13 @@ class Deliveryorder extends AppModel
             'fields' => '',
             'order' => ''
         ),
+        'Quotation' => array(
+            'className' => 'Quotation',
+            'foreignKey' => 'quotationno',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
         'Invoice' => array(
             'className' => 'Invoice',
             'foreignKey' => 'delivery_order_no',
@@ -44,6 +52,13 @@ class Deliveryorder extends AppModel
          'branch' => array(
             'className' => 'branch',
             'foreignKey' => 'branch_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'InstrumentType' => array(
+            'className' => 'InstrumentType',
+            'foreignKey' => 'instrument_type_id',
             'conditions' => '',
             'fields' => '',
             'order' => ''
